a0994dd816686d816b9bd7fea39f0b1012443246
oxo970311 <148974384+oxo970311@users.noreply.github.com>
2025-07-23 16:37:45 +0900

    거북이 모터&버튼 이동
    거북이 모터&버튼 이동

--- Turtle_Servo_Motor.py
+++ Turtle_Servo_Motor.py
@@ -37,4 +37,37 @@ except KeyboardInterrupt:
 finally:
     ser.close()
 
-# turtle.done()
+# turtle.done
+
+# Arduino Code
+'''
+#include <Servo.h>
+
+Servo myservo;
+int pos = 0;
+int button = 10;
+int buttonState = 0;
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(7);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+buttonState = digitalRead(button);
+if (buttonState == HIGH) {
+    for (pos = 0; pos <= 180; pos++) {
+    myservo.write(pos);
+    Serial.println(pos);
+    delay(15);
+  }
+
+    for (pos = 180; pos >= 0; pos--) {
+    myservo.write(pos);
+    Serial.println(pos);
+    delay(15);
+    }
+  }
+}
+'''
